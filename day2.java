7bfb978949a528d16f106a185dc904f55ae29f16
saipraseeda1025-collab <saipraseeda.1025@gmail.com>
2026-01-06 19:53:10 +0530

    Add Java program to calculate sum and average

---
+++ day2.java
@@ -0,0 +1,13 @@
+public class day2 {
+    public static void main(String[]args){
+        int arr[]={10,20,30,40,50};
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+sum+=arr[i];
+        }
+        double avg=(double)sum/arr.length;
+    System.out.println("sum"+sum);
+    System.out.println("Avg"+avg);
+    }
+    
+}
