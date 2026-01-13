91880f9ca18daf02ef7e62728ab8b09d091f45f0
saipraseeda1025-collab <saipraseeda.1025@gmail.com>
2026-01-13 23:33:59 +0530

    Add files via upload

---
+++ Day7.java
@@ -0,0 +1,12 @@
+public class Day7{
+    public static void main(String[]args){
+        int a[]={10,20,30,40,50};
+        int key=30;
+        for(int i=0;i<a.length;i++){
+            if(a[i]==key){
+                System.out.println("Found at index"+i);
+                break;
+            }
+        }
+    }
+}
